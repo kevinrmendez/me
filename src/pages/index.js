4d911ba0ee2b307b86b143ce17a00384e765fee6
Kevin Mendez <kevinrmendez@gmail.com>
2019-08-08 01:36:32 +0300

    typewritting effect on title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import TypeWriter from "../components/typewriter"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Front end Developer</h1>
+    <TypeWriter title="Kevin Mendez Front End Developer!" speed={110}/>
+
+
 
     <p style={{fontSize:40, textAlign:`center`}}>I am Kevin and I am a front end developer</p>
     <div style={{ maxWidth: `700px`, marginBottom: `1.45rem`, margin:`0 auto` }}>
