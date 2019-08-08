de0e736e2749724055ef9f5f35475f6ae19d3a04
Kevin Mendez <kevinrmendez@gmail.com>
2019-08-08 16:23:10 +0300

    form changed

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -64,18 +64,15 @@ const AboutMe = ({data}) => (
     </Row>
     <Row>
     <Col>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" etlify-honeypot="bot-field" data-netlify="true">
+    <p css={css`display:none;`} class="hidden">
+    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+  </p>
 <p>
 <label>Your Name: <input type="text" name="name" /></label>
 </p>
 <p>
 <label>Your Email: <input type="email" name="email" /></label>
-</p>
-<p>
-<label>Your Role: <select name="role[]" multiple>
-  <option value="leader">Leader</option>
-  <option value="follower">Follower</option>
-</select></label>
 </p>
 <p>
 <label>Message: <textarea name="message"></textarea></label>
