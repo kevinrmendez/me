cbba9827158464d6c425947446b3f11e24a17ca9
Kevin Mendez <kevinrmendez@gmail.com>
2019-08-08 16:49:51 +0300

    form added

--- src/components/form.js
+++ src/components/form.js
@@ -15,7 +15,7 @@ export default () => (
     </div>
     <div>
       <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" />
+      <input type="email" name="email" id="email" />
     </div>
     <div>
       <label htmlFor="message">Message</label>
