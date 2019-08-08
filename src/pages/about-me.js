28e70f6a904b938ff5d004808b24a0de79c8381e
Kevin Mendez <kevinrmendez@gmail.com>
2019-08-08 12:57:20 +0300

    change index style

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -8,6 +8,9 @@ import {css} from "@emotion/core"
 import styled from "@emotion/styled"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Icon from "../assets/git.svg"
+import { Container, Row, Col } from 'react-grid-system';
+
+
 export const data = graphql`
 query {
    file(relativePath: { eq:"in2.png" }) {
@@ -25,8 +28,12 @@ const AboutMe = ({data}) => (
     <SEO title="About me" />
     <h1>About me</h1>
     <p class="text-big">I like to learn about new technologies </p>
+
+    <Container>
+    <Row justify="center">
+    <Col>
     <p> Here you can check my github</p>
-    <a href="https://github.com/kevinrmendez">
+    <a href={"https://github.com/kevinrmendez"}>
       <Icon css={css`
             max-width:170px;
             background: #fff;
@@ -35,12 +42,21 @@ const AboutMe = ({data}) => (
             padding: 9px;
         `} />
     </a>
-    <a href="https://www.linkedin.com/in/kevin-mendez-45723692/">
+    </Col>
+    <Col>
+    <p> Here you can check my linkedin</p>
+    <a href={"https://www.linkedin.com/in/kevin-mendez-45723692/"}>
       <Img fixed={data.file.childImageSharp.fixed} />
     </a>
+    </Col>
 
-
+    </Row>
+    </Container>
   </Layout>
 )
+
+const Logo = ()=> {
+
+}
 
 export default AboutMe
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import {css} from "@emotion/core"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -12,8 +12,52 @@ const IndexPage = () => (
 
 
 
-    <p style={{fontSize:40, textAlign:`center`}}>I am Kevin and I am a front end developer</p>
-    <div style={{ maxWidth: `700px`, marginBottom: `1.45rem`, margin:`0 auto` }}>
+    <p css={
+      css`
+      font-size: 40px;
+      text-align: center;
+      background: white;
+      color: black;
+      border-radius: 74px;
+      position: absolute;
+      left: 48%;
+      top: 500px;
+      z-index: 1;
+      max-width: 400px;
+      font-size: 30px;
+      padding: 23px;
+
+}
+      `
+    }>
+    <span css={
+      css`
+      height: 60px;
+  width: 60px;
+  display: block;
+  background: #fff;
+  position: absolute;
+  bottom: -36px;
+left: 0;
+clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+transform: rotate(210deg);
+
+
+
+
+
+      `
+    } />
+    I am maste degree student in software development in Oulu University</p>
+
+
+    <div css={css`
+              max-width: 700px;
+              margin-bottom:1.45rem;
+              margin:0 auto;
+              margin-top: 200px;
+
+      `}>
       <Image />
     </div>
 
