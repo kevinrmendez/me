c68318103de450ff3f756bbaef42eec3ab0c4475
Kevin Mendez <kevinrmendez@gmail.com>
2019-08-09 15:57:15 +0300

    changes layout

---
+++ src/components/dialog.js
@@ -0,0 +1,40 @@
+import React from "react"
+import {css} from '@emotion/core'
+
+const Dialog = (props) => {
+
+
+  return <p className="dialog fade-in" css={css`
+    font-size: 40px;
+    text-align: center;
+    background: white;
+    color: black;
+    border-radius: 74px;
+    position: absolute;
+    left: 48%;
+    top: 470px;
+    z-index: 1;
+    max-width: 360px;
+    font-size: 30px;
+    padding: 40px;
+    /* min-width: 215px; */
+    `}  >
+    <span css={
+      css`
+      height: 60px;
+  width: 60px;
+  display: block;
+  background: #fff;
+  position: absolute;
+  bottom: -36px;
+left: 0;
+clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+transform: rotate(210deg);
+      `
+    } />
+        {props.text}
+    </p>
+
+}
+
+export default  Dialog
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import {css} from "@emotion/core"
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Dialog from "../components/dialog"
 import SEO from "../components/seo"
 import TypeWriter from "../components/typewriter"
 const IndexPage = () => (
@@ -11,39 +12,7 @@ const IndexPage = () => (
 
     <TypeWriter className="front-page" title="Kevin Mendez Front End Developer!" speed={110}/>
 
-
-
-    <p css={
-      css`
-      font-size: 40px;
-      text-align: center;
-      background: white;
-      color: black;
-      border-radius: 74px;
-      position: absolute;
-      left: 48%;
-      top: 470px;
-      z-index: 1;
-      max-width: 360px;
-      font-size: 30px;
-      padding: 40px;
-}
-      `
-    }>
-    <span css={
-      css`
-      height: 60px;
-  width: 60px;
-  display: block;
-  background: #fff;
-  position: absolute;
-  bottom: -36px;
-left: 0;
-clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
-transform: rotate(210deg);
-      `
-    } />
-    I am a front end developer living in Finland</p>
+    <Dialog text="I am a front end developer living in Finland" />
 
 
     <div css={css`
